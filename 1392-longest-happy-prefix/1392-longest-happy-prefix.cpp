bed07381db85cd41754be464fa5de146a5367852
rahuldhami1703-blip <rahuldhami1703@gmail.com>
2026-09-19 22:10:33 +0530

    Time: 8 ms (54.46%), Space: 23.2 MB (79.99%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     string longestPrefix(string s) {
-      int n = s.size();
-      int i=1 , length = 0 ;
-      vector<int>lps(n,0) ;
+        int n = s.size();
+        int i = 1 , index = 0 ;
+        vector<int>lps( n,index );
 
-      while( i<n ){
-        if( s[i]==s[length] ){
-            length+=1;
-            lps[i]=length ;
-            i+=1;
-        }else{
-            if( length!=0 ){
-                length= lps[length-1];
+        while( i<n ){
+            if( s[i]==s[index] ){
+                index += 1 ;
+                lps[i] = index ;
+                i += 1 ;
             }else{
-                lps[i]=0 ;
-                i+=1 ;
+                if( index != 0 ){
+                    index = lps[index-1] ;
+                }else{
+                    lps[i] = 0 ;
+                    i += 1 ;
+                }
             }
         }
-      }
-      string ans = "";  
-      ans = s.substr( 0,length ) ;
-      return ans ;
+        string ans = "" ;
+        ans = s.substr( 0,index ) ;
+        return ans ;
     }
 };
